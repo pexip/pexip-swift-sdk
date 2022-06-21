ef34c1c98ed74c9f5d04914bb38fbf69130c1cab
Vadym Markov <markov.vadym@gmail.com>
2022-06-21 21:08:31 +0200

    Update sdk pages

--- assets/header.js
+++ assets/header.js
@@ -1,12 +1,9 @@
-(() => {
-    const shadowRoot = document.querySelector('custom-header').shadowRoot;
-    const header = shadowRoot.getElementById('header');
-    const link = document.createElement('link');
-
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('href', '/pexip-swift-sdk/assets/header.css');
-    link.onload = () => {
-      header.hidden = false;
-    };
-    shadowRoot.appendChild(link);
-  })();
+function myFunction() {
+  var x = document.getElementById("menu");
+  
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
