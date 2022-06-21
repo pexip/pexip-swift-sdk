77bf55d669575f4548f28623ca11fc2af9166f97
Vadym Markov <markov.vadym@gmail.com>
2022-06-21 22:42:32 +0200

    Update sdk and framework docs

--- assets/header.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  var x = document.getElementById("menu");
-  
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
